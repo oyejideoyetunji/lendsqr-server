c37f0bb2182b1770cc114ea6c9a18a0e08b219ff
oluwatobi oyejide <moyejide2016@gmail.com>
2022-03-14 01:15:04 +0100

    server-deploy

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
 
-const publicPath = path.join(__dirname, '..', 'build');
+
 const port = process.env.PORT || 8000;
 
 
 app.use(express.static("build"))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
+app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
 app.listen(port, () => {
